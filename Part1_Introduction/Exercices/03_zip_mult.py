9be993d8c87de28d1d534f3a137962a861748573
Antoine07 <antoine.lucsko@gmail.com>
2023-02-07 19:35:37 +0100

    corrections

--- Part1_Introduction/Exercices/03_zip_mult.py
+++ Part1_Introduction/Exercices/03_zip_mult.py
@@ -1,3 +1,5 @@
+
+# on peut faire son propre générateur ou utiliser la fonction zip de Python
 def myZip(l, m):
     assert len(l) == len(m)
     i = 0
@@ -8,7 +10,9 @@ def myZip(l, m):
 l = [1, 2]
 m = [10, 20]
 
+# version pythonique de la somme
+print(sum(a * b for a, b in myZip(l, m)))
 
-print( sum( a * b for a, b in  myZip(l, m) ) )
-
-
+z = myZip(l, m)
+print(next(z))
+print(next(z))
